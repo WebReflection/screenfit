67edec2c6d1717723d1acfb3f16793619d577133
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-11-16 14:55:47 +0100

    removed orientationchange as it is useless

--- esm/index.js
+++ esm/index.js
@@ -2,7 +2,6 @@
 const {
   CustomEvent,
   addEventListener,
-  clearTimeout,
   dispatchEvent,
   document,
   scrollTo,
@@ -14,23 +13,15 @@ const detail = visualViewport || {
   get width() { return self.innerWidth; },
   get height() { return self.innerHeight; }
 };
-const OC = 'orientationchange';
-const later = () => {
-  dispatchEvent(new CustomEvent('screenfit', {detail}));
-  return 0;
-};
-const resize = ({type}) => {
+const resize = () => {
   document.body.style.height = detail.height + 'px';
   scrollTo(0, 0);
   // Needed for Windows Phone
   setTimeout(scrollTo, 300, 0, 0);
-  clearTimeout(timer);
-  timer = type === OC ? setTimeout(later, 50) : later();
+  dispatchEvent(new CustomEvent('screenfit', {detail}));
 };
-let timer = 0;
 document.head.appendChild(document.createElement('style')).textContent =
 'html,body{padding:0;margin:0;overflow:hidden;box-sizing:border-box;\
 height:100%;height:100vh;height:-moz-available;height:-webkit-fill-available}';
 document.addEventListener('DOMContentLoaded', resize, {once: true});
-addEventListener(OC, resize);
 detail.addEventListener('resize', resize);
--- index.js
+++ index.js
@@ -5,7 +5,6 @@
   var _self = self,
       CustomEvent = _self.CustomEvent,
       addEventListener = _self.addEventListener,
-      clearTimeout = _self.clearTimeout,
       dispatchEvent = _self.dispatchEvent,
       document = _self.document,
       scrollTo = _self.scrollTo,
@@ -23,32 +22,22 @@
     }
 
   };
-  var OC = 'orientationchange';
 
-  var later = function later() {
-    dispatchEvent(new CustomEvent('screenfit', {
-      detail: detail
-    }));
-    return 0;
-  };
-
-  var resize = function resize(_ref) {
-    var type = _ref.type;
+  var resize = function resize() {
     document.body.style.height = detail.height + 'px';
     scrollTo(0, 0); // Needed for Windows Phone
 
     setTimeout(scrollTo, 300, 0, 0);
-    clearTimeout(timer);
-    timer = type === OC ? setTimeout(later, 50) : later();
+    dispatchEvent(new CustomEvent('screenfit', {
+      detail: detail
+    }));
   };
 
-  var timer = 0;
   document.head.appendChild(document.createElement('style')).textContent = 'html,body{padding:0;margin:0;overflow:hidden;box-sizing:border-box;\
 height:100%;height:100vh;height:-moz-available;height:-webkit-fill-available}';
   document.addEventListener('DOMContentLoaded', resize, {
     once: true
   });
-  addEventListener(OC, resize);
   detail.addEventListener('resize', resize);
 
 }());
