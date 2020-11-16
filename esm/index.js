b0a85486884c043a62949b1b28a961348ed814aa
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-11-16 09:21:53 +0100

    better delay logic for screenfit event

--- esm/index.js
+++ esm/index.js
@@ -1,22 +1,42 @@
 /*! (c) Andrea Giammarchi - ISC */
-const {addEventListener, document, visualViewport} = self;
+const {
+  CustomEvent,
+  addEventListener,
+  cancelAnimationFrame,
+  clearTimeout,
+  dispatchEvent,
+  document,
+  requestAnimationFrame,
+  scrollTo,
+  setTimeout,
+  visualViewport
+} = self;
 const detail = visualViewport || {
   addEventListener: addEventListener.bind(self),
   get width() { return self.innerWidth; },
   get height() { return self.innerHeight; }
 };
+const later = () => {
+  dispatchEvent(new CustomEvent('screenfit', {detail}));
+};
+const resize = () => {
+  document.body.style.height = detail.height + 'px';
+  scrollTo(0, 0);
+  // Needed for Windows Phone
+  setTimeout(scrollTo, 300, 0, 0);
+  if (cancelAnimationFrame) {
+    cancelAnimationFrame(timer);
+    timer = requestAnimationFrame(later);
+  }
+  else {
+    clearTimeout(timer);
+    timer = setTimeout(later);
+  }
+};
+let timer = 0;
 document.head.appendChild(document.createElement('style')).textContent =
 'html,body{padding:0;margin:0;overflow:hidden;box-sizing:border-box;\
 height:100%;height:100vh;height:-moz-available;height:-webkit-fill-available}';
 document.addEventListener('DOMContentLoaded', resize, {once: true});
 addEventListener('orientationchange', resize);
 detail.addEventListener('resize', resize);
-let timer = 0;
-function resize() {
-  document.body.style.height = detail.height + 'px';
-  scrollTo(0, 0);
-  // Needed for Windows Phone
-  setTimeout(scrollTo, 300, 0, 0);
-  clearTimeout(timer);
-  timer = setTimeout(dispatchEvent, 0, new CustomEvent('screenfit', {detail}));
-}
--- index.js
+++ index.js
@@ -3,8 +3,15 @@
 
   /*! (c) Andrea Giammarchi - ISC */
   var _self = self,
+      CustomEvent = _self.CustomEvent,
       addEventListener = _self.addEventListener,
+      cancelAnimationFrame = _self.cancelAnimationFrame,
+      clearTimeout = _self.clearTimeout,
+      dispatchEvent = _self.dispatchEvent,
       document = _self.document,
+      requestAnimationFrame = _self.requestAnimationFrame,
+      scrollTo = _self.scrollTo,
+      setTimeout = _self.setTimeout,
       visualViewport = _self.visualViewport;
   var detail = visualViewport || {
     addEventListener: addEventListener.bind(self),
@@ -18,6 +25,29 @@
     }
 
   };
+
+  var later = function later() {
+    dispatchEvent(new CustomEvent('screenfit', {
+      detail: detail
+    }));
+  };
+
+  var resize = function resize() {
+    document.body.style.height = detail.height + 'px';
+    scrollTo(0, 0); // Needed for Windows Phone
+
+    setTimeout(scrollTo, 300, 0, 0);
+
+    if (cancelAnimationFrame) {
+      cancelAnimationFrame(timer);
+      timer = requestAnimationFrame(later);
+    } else {
+      clearTimeout(timer);
+      timer = setTimeout(later);
+    }
+  };
+
+  var timer = 0;
   document.head.appendChild(document.createElement('style')).textContent = 'html,body{padding:0;margin:0;overflow:hidden;box-sizing:border-box;\
 height:100%;height:100vh;height:-moz-available;height:-webkit-fill-available}';
   document.addEventListener('DOMContentLoaded', resize, {
@@ -25,17 +55,5 @@ height:100%;height:100vh;height:-moz-available;height:-webkit-fill-available}';
   });
   addEventListener('orientationchange', resize);
   detail.addEventListener('resize', resize);
-  var timer = 0;
-
-  function resize() {
-    document.body.style.height = detail.height + 'px';
-    scrollTo(0, 0); // Needed for Windows Phone
-
-    setTimeout(scrollTo, 300, 0, 0);
-    clearTimeout(timer);
-    timer = setTimeout(dispatchEvent, 0, new CustomEvent('screenfit', {
-      detail: detail
-    }));
-  }
 
 }());
